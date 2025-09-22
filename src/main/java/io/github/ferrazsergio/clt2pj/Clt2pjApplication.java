85abc86b627f5a1f7f9cfde9eb5f72f7326f8781
sergio-s-junior-opl <sergio-s-junior@openlabs.com.br>
2025-09-22 12:12:06 -0300

    Estrutura inicial do projeto

---
+++ src/main/java/io/github/ferrazsergio/clt2pj/Clt2pjApplication.java
@@ -0,0 +1,13 @@
+package io.github.ferrazsergio.clt2pj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Clt2pjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Clt2pjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/ferrazsergio/clt2pj/Clt2pjApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ferrazsergio.clt2pj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Clt2pjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
